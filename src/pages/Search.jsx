ce2b1bc11e473b2c13efe0b1000bd55a8562abc0
Guidzs <guidz.2004@gmail.com>
2022-08-01 20:32:53 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,47 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    bandName: '',
+    buttonDisabled: true,
+  }
+
+  ValidateName = (event) => {
+    const { value } = event.target;
+
+    this.setState({ bandName: value }, () => {
+      this.setState(() => {
+        const { bandName } = this.state;
+        const MIN_LENGTH = 2;
+        const buttonValidate = bandName.length >= MIN_LENGTH;
+        return {
+          buttonDisabled: !buttonValidate,
+        };
+      });
+    });
+  }
+
   render() {
+    const { buttonDisabled } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            name="band-name"
+            onChange={ this.ValidateName }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ buttonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
